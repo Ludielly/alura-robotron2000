db136c5f68f5fca7c61b7795a7116c64cb5dd2ad
Ludielly Silveira <ludysilveira02@gmail.com>
2022-11-23 13:14:42 -0300

    terminando a parte 3 do curso JS: manipulando o DOM

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
- 
+const subtrair = document.querySelector('#subtrair') 
+const braco = document.querySelector('#braco')
+const somar = document.querySelector('#somar')
+const controle = document.querySelectorAll('.controle-ajuste')
+
+controle.forEach( (elemento) => {
+    elemento.addEventListener('click', (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+})
+
+function manipulaDados(operacao) {
+    if(operacao == '+') {
+        braco.value = parseInt(braco.value) + 1
+    }
+    else {
+        braco.value = parseInt(braco.value) - 1
+    }
+}
